10979282246a84e988c19731d7959570e8a1c290
chunqian.shen <chunqian.shen@qq.com>
2022-08-15 12:07:44 +0800

    f

--- cronolog.go
+++ cronolog.go
@@ -1,9 +1,10 @@
-//go:build !cronolog
+//go:build cronolog
 
 /**---------------------------------------------------------
- * name: log.go
- * author: shenchunqian
- * created: 2022-08-12
+ * name: writer.go
+ * editor: shenchunqian
+ * created: 2022-08-15
+ * source: https://github.com/utahta/go-cronowriter
  ---------------------------------------------------------*/
 
 package log
@@ -11,12 +12,16 @@ package log
 import (
 	"bytes"
 	"fmt"
-	"github.com/chunqian/tinylog/pretty"
+	"io"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/chunqian/tinylog/pretty"
+	"github.com/lestrrat-go/strftime"
 )
 
 var (
@@ -30,6 +35,8 @@ var (
 	DefaultCallerDepth = 3
 
 	levelFlags = []string{"DEBUG", "INFO", "WARN", "ERROR", "FATAL"}
+
+	writer *Writer
 )
 
 func init() {
@@ -39,6 +46,9 @@ func init() {
 	ShowDepth = false
 	ShowTime = false
 	ShowPrefix = false
+
+	getwd, _ := os.Getwd()
+	writer, _ = NewWriter(getwd + "/logs/%Y/%m/%d/tiny.log")
 }
 
 type Level int
@@ -50,6 +60,132 @@ const (
 	ERROR
 	FATAL
 )
+
+type (
+	// A Writer writes message to a set of output files.
+	Writer struct {
+		pattern *strftime.Strftime // given pattern
+		path    string             // current file path
+		symlink *strftime.Strftime // symbolic link to current file path
+		fp      *os.File           // current file pointer
+		loc     *time.Location
+		mux     sync.Locker
+		init    bool // if true, open the file when New() method is called
+	}
+)
+
+var (
+	_   io.WriteCloser = (*Writer)(nil) // check if object implements interface
+	now                = time.Now       // for test
+)
+
+// New returns a Writer with the given pattern.
+func NewWriter(pattern string) (*Writer, error) {
+	p, err := strftime.New(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Writer{
+		pattern: p,
+		path:    "",
+		symlink: nil,
+		fp:      nil,
+		loc:     time.Local,
+		mux:     new(sync.Mutex), // default mutex enable
+		init:    false,
+	}
+
+	if c.init {
+		if _, err := c.Write([]byte("")); err != nil {
+			return nil, err
+		}
+	}
+	return c, nil
+}
+
+// Write writes to the file and rotate files automatically based on current date and time.
+func (c *Writer) Write(b []byte) (int, error) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	t := now().In(c.loc)
+	path := c.pattern.FormatString(t)
+
+	if c.path != path {
+		// close file
+		go func(fp *os.File) {
+			if fp == nil {
+				return
+			}
+			fp.Close()
+		}(c.fp)
+
+		dir := filepath.Dir(path)
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return c.write(nil, err)
+		}
+
+		fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			return c.write(nil, err)
+		}
+		c.createSymlink(t, path)
+
+		c.path = path
+		c.fp = fp
+	}
+
+	return c.write(b, nil)
+}
+
+// Path returns the current writing file path.
+func (c *Writer) Path() string {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.path
+}
+
+func (c *Writer) createSymlink(t time.Time, path string) {
+	if c.symlink == nil {
+		return
+	}
+
+	symlink := c.symlink.FormatString(t)
+	if symlink == path {
+		fmt.Println("Can't create symlink. Already file exists.")
+		return // ignore error
+	}
+
+	if _, err := os.Stat(symlink); err == nil {
+		if err := os.Remove(symlink); err != nil {
+			fmt.Println(err)
+			return // ignore error
+		}
+	}
+
+	if err := os.Symlink(path, symlink); err != nil {
+		fmt.Println(err)
+		return // ignore error
+	}
+}
+
+// Close closes file.
+func (c *Writer) Close() error {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	return c.fp.Close()
+}
+
+func (c *Writer) write(b []byte, err error) (int, error) {
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+
+	return c.fp.Write(b)
+}
 
 func Print(level Level, depth int, addNewline bool, args ...interface{}) {
 	var buf bytes.Buffer
@@ -158,7 +294,8 @@ func Print(level Level, depth int, addNewline bool, args ...interface{}) {
 	selected = append(selected, buf.String())
 	formatBuf.WriteString("\n")
 
-	fmt.Printf(formatBuf.String(), selected...)
+	// fmt.Printf(formatBuf.String(), selected...)
+	writer.Write([]byte(fmt.Sprintf(formatBuf.String(), selected...)))
 
 	if level == FATAL {
 		os.Exit(1)
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/chunqian/tinylog
 go 1.19
 
 require (
-	github.com/kr/text v0.2.0 // indirect
-	github.com/rogpeppe/go-internal v1.8.1 // indirect
+	github.com/kr/text v0.2.0
+	github.com/lestrrat-go/strftime v1.0.6
 )
+
+require github.com/pkg/errors v0.9.1 // indirect
